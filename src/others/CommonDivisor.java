2dc97649af0306dcb1b4393c56673e3bb467a145
yupan03 <735997736@qq.com>
2019-12-06 08:46:49 +0800

    忽略文件和增加其他算法

---
+++ src/others/CommonDivisor.java
@@ -0,0 +1,40 @@
+package others;
+
+public class CommonDivisor {
+    public static void main(String[] args) {
+        System.out.println(greatestCommonDivisor(100, 1020));
+        System.out.println(getMaxNum(12));
+    }
+
+    /**
+     * 欧几米得算法求最大公约数
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int greatestCommonDivisor(int x, int y) {
+        if (y == 0) return x;
+        return greatestCommonDivisor(y, x % y);
+    }
+
+    /**
+     * 空瓶子换饮料的算�?
+     *
+     * @param n
+     * @return
+     */
+    public static int getMaxNum(int n) {
+        int k = 0;
+
+        while (n / 3 > 0) {
+            k += n / 3;
+            n = n / 3 + n % 3;
+        }
+        if (n % 3 == 2) k += 1;
+
+        return k;
+    }
+
+
+}
