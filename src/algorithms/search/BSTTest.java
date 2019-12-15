11ee2791ba70c75e95bdb520964a064ffeae1cef
yupan03 <735997736@qq.com>
2019-12-15 10:51:35 +0800

    提交代码

--- src/algorithms/search/BSTTest.java
+++ src/algorithms/search/BSTTest.java
@@ -3,13 +3,13 @@ package algorithms.search;
 public class BSTTest {
 
     public static void main(String[] args) {
-        BST<Long, String> bst = new BST<Long, String>(3L, "0");
+        BST<Long, String> bst = new BST<Long, String>();
 
-        bst.put(1L, "3");
-        bst.put(0L, "3");
-        bst.put(2L, "3");
-        bst.put(4L, "3");
-        bst.put(5L, "3");
+        bst.put(1L, "1");
+        bst.put(2L, "2");
+        bst.put(4L, "4");
+        bst.put(5L, "5");
+        bst.put(0L, "0");
 
         System.out.print("先序遍历：");
         bst.recursivePreOrder();
@@ -24,7 +24,10 @@ public class BSTTest {
         System.out.println("树大小:" + bst.size());
         System.out.println("最小值:" + bst.min());
         System.out.println("最大值:" + bst.max());
-        System.out.println("向上取数:" + bst.floor((long) 2.5));
-        System.out.println("向下取数:" + bst.ceil((long) 0.5));
+        System.out.println("向上取数2.5:" + bst.floor((long) 2.5));
+        System.out.println("向下取数0.5:" + bst.ceil((long) 0.5));
+        
+        System.out.println("指定位置的元素3："+ bst.select(3));
+        System.out.println("元素3的位置:" + bst.rank(3L));
     }
 }
