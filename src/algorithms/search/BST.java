deab777c1861a19a0eb827281474aab4d7715297
yupan03 <735997736@qq.com>
2019-12-16 21:29:31 +0800

    类型错误

--- src/algorithms/search/BST.java
+++ src/algorithms/search/BST.java
@@ -2,231 +2,251 @@ package algorithms.search;
 
 public class BST<Key extends Comparable<Key>, Value> {
 
-	private class Node {
-		private Key key;
-		private Value value;
-		private Node left, right;
-		private int N; // 该节点下的所有子节点数量（包含自身）
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left, right;
+        private int N; // 该节点下的所有子节点数量（包含自身）
 
-		public Node(Key key, Value value, int N) {
-			this.key = key;
-			this.value = value;
-			this.N = N;
-		}
-	}
+        public Node(Key key, Value value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+        }
+    }
 
-	private Node root;
+    private Node root;
 
-	public BST() {
-	}
+    public BST() {
+    }
 
-	public void recursivePreOrder() {
-		recursivePreOrder(root);
-	}
+    public void recursivePreOrder() {
+        recursivePreOrder(root);
+    }
 
-	/**
-	 * 先序遍历
-	 * 
-	 * @param node
-	 */
-	private void recursivePreOrder(Node node) {
-		if (node == null)
-			return;
-		System.out.print(node.key + " ");
-		recursivePreOrder(node.left);
-		recursivePreOrder(node.right);
-	}
+    /**
+     * 先序遍历
+     * 
+     * @param node
+     */
+    private void recursivePreOrder(Node node) {
+        if (node == null)
+            return;
+        System.out.print(node.key + " ");
+        recursivePreOrder(node.left);
+        recursivePreOrder(node.right);
+    }
 
-	public void recursiveInOrder() {
-		recursiveInOrder(root);
-	}
+    public void recursiveInOrder() {
+        recursiveInOrder(root);
+    }
 
-	/**
-	 * 中序遍历
-	 * 
-	 * @param node
-	 */
-	private void recursiveInOrder(Node node) {
-		if (node == null)
-			return;
-		recursiveInOrder(node.left);
-		System.out.print(node.key + " ");
-		recursiveInOrder(node.right);
-	}
+    /**
+     * 中序遍历
+     * 
+     * @param node
+     */
+    private void recursiveInOrder(Node node) {
+        if (node == null)
+            return;
+        recursiveInOrder(node.left);
+        System.out.print(node.key + " ");
+        recursiveInOrder(node.right);
+    }
 
-	public void recursivePostOrder() {
-		recursivePostOrder(root);
-	}
+    public void recursivePostOrder() {
+        recursivePostOrder(root);
+    }
 
-	/**
-	 * 后序遍历
-	 * 
-	 * @param node
-	 */
-	private void recursivePostOrder(Node node) {
-		if (node == null)
-			return;
-		recursivePostOrder(node.left);
-		recursivePostOrder(node.right);
-		System.out.print(node.key + " ");
-	}
+    /**
+     * 后序遍历
+     * 
+     * @param node
+     */
+    private void recursivePostOrder(Node node) {
+        if (node == null)
+            return;
+        recursivePostOrder(node.left);
+        recursivePostOrder(node.right);
+        System.out.print(node.key + " ");
+    }
 
-	/**
-	 * 二叉树大小
-	 * 
-	 * @return
-	 */
-	public int size() {
-		return size(root);
-	}
+    /**
+     * 二叉树大小
+     * 
+     * @return
+     */
+    public int size() {
+        return size(root);
+    }
 
-	/**
-	 * 节点大小
-	 * 
-	 * @param node
-	 * @return
-	 */
-	private int size(Node node) {
-		if (node == null)
-			return 0;
-		return node.N;
-	}
+    /**
+     * 节点大小
+     * 
+     * @param node
+     * @return
+     */
+    private int size(Node node) {
+        if (node == null)
+            return 0;
+        return node.N;
+    }
 
-	public Value get(Key key) {
-		return get(root, key);
-	}
+    public Value get(Key key) {
+        return get(root, key);
+    }
 
-	private Value get(Node node, Key key) {
-		if (node == null)
-			return null;
-		int cmp = key.compareTo(node.key);
-		if (cmp < 0)
-			return get(node.left, key);
-		if (cmp > 0)
-			return get(node.right, key);
+    private Value get(Node node, Key key) {
+        if (node == null)
+            return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            return get(node.left, key);
+        if (cmp > 0)
+            return get(node.right, key);
 
-		return node.value;
+        return node.value;
 
-	}
+    }
 
-	public void put(Key key, Value value) {
-		root = put(root, key, value);
-	}
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
 
-	private Node put(Node node, Key key, Value value) {
-		if (node == null)
-			return new Node(key, value, 1);
+    private Node put(Node node, Key key, Value value) {
+        if (node == null)
+            return new Node(key, value, 1);
 
-		int cmp = key.compareTo(node.key);
-		if (cmp < 0)
-			node.left = put(node.left, key, value);
-		if (cmp > 0)
-			node.right = put(node.right, key, value);
-		else
-			node.value = value;
-		node.N = size(node.left) + size(node.right) + 1;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
+        node.N = size(node.left) + size(node.right) + 1;
 
-		return node;
-	}
+        return node;
+    }
 
-	public Key min() {
-		return min(root).key;
-	}
+    public Key min() {
+        return min(root).key;
+    }
 
-	private Node min(Node node) {
-		if (node.left == null)
-			return node;
-		return min(node.left);
-	}
+    private Node min(Node node) {
+        if (node.left == null)
+            return node;
+        return min(node.left);
+    }
 
-	public Key max() {
-		return max(root).key;
-	}
+    public Key max() {
+        return max(root).key;
+    }
 
-	private Node max(Node node) {
-		if (node.right == null)
-			return node;
-		return max(node.right);
-	}
+    private Node max(Node node) {
+        if (node.right == null)
+            return node;
+        return max(node.right);
+    }
 
-	public Key floor(Key key) {
-		return floor(key, root).key;
-	}
+    public Key floor(Key key) {
+        return floor(key, root).key;
+    }
 
-	private Node floor(Key key, Node node) {
-		if (node == null)
-			return null;
-		int cmp = key.compareTo(node.key);
+    private Node floor(Key key, Node node) {
+        if (node == null)
+            return null;
+        int cmp = key.compareTo(node.key);
 
-		if (cmp == 0) {
-			return node;
-		} else if (cmp < 0) {
-			return floor(key, node.left);
-		}
-		Node t = floor(key, node.right);
+        if (cmp == 0) {
+            return node;
+        } else if (cmp < 0) {
+            return floor(key, node.left);
+        } else {
+            Node t = floor(key, node.right);
+            if (t == null) {
+                return node;
+            } else {
+                return t;
+            }
+        }
+    }
 
-		if (t == null) {
-			return node;
-		} else {
-			return t;
-		}
-	}
+    public Key ceil(Key key) {
+        if (key == null)
+            return null;
+        return ceil(key, root).key;
+    }
 
-	public Key ceil(Key key) {
-		if (key == null)
-			return null;
-		return ceil(key, root).key;
-	}
+    private Node ceil(Key key, Node node) {
+        if (node == null)
+            return null;
+        int cmp = key.compareTo(node.key);
 
-	private Node ceil(Key key, Node node) {
-		if (node == null)
-			return null;
-		int cmp = key.compareTo(node.key);
+        if (cmp == 0) {
+            return node;
+        } else if (cmp > 0) {
+            return ceil(key, node.right);
+        }
 
-		if (cmp == 0) {
-			return node;
-		} else if (cmp < 0) {
-			Node t = ceil(key, node.left);
+        Node t = ceil(key, node.left);
 
-			if(t == null) return node;
-			else return t;
-		}
+        if (t != null) {
+            return t;
+        } else {
+            return node;
+        }
 
-		return ceil(key, node.right);
-	}
+    }
 
-	public Key select(int k) {
-		return select(k, root).key;
-	}
+    public Key select(int k) {
+        return select(k, root).key;
+    }
 
-	private Node select(int k, Node node) {
-		if (node == null)
-			return null;
-		int t = size(node.left);
+    private Node select(int k, Node node) {
+        if (node == null)
+            return null;
+        int t = size(node.left);
 
-		if (t > k)
-			return select(k, node.left);
-		else if (t < k)
-			return select(k - t - 1, node.right);
-		else
-			return node;
-	}
+        if (t > k)
+            return select(k, node.left);
+        else if (t < k)
+            return select(k - t - 1, node.right);
+        else
+            return node;
+    }
 
-	public int rank(Key key) {
-		return rank(key, root);
-	}
+    public int rank(Key key) {
+        return rank(key, root);
+    }
 
-	private int rank(Key key, Node node) {
-		if (node == null)
-			return 0;
+    private int rank(Key key, Node node) {
+        if (node == null)
+            return 0;
 
-		int cmp = key.compareTo(node.key);
+        int cmp = key.compareTo(node.key);
 
-		if (cmp == 0)
-			return size(node.left);
-		else if (cmp < 0)
-			return rank(key, node.left);
-		else
-			return 1 + size(node.left) + rank(key, node.right);
-	}
+        if (cmp == 0)
+            return size(node.left);
+        else if (cmp < 0)
+            return rank(key, node.left);
+        else
+            return 1 + size(node.left) + rank(key, node.right);
+    }
+
+    public int heigh() {
+        return heigh(root);
+    }
+
+    private int heigh(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int l = heigh(node.left);
+
+        int r = heigh(node.right);
+
+        return l > r ? (1 + l) : (1 + r);
+    }
 }
--- src/algorithms/search/BSTTest.java
+++ src/algorithms/search/BSTTest.java
@@ -3,13 +3,13 @@ package algorithms.search;
 public class BSTTest {
 
     public static void main(String[] args) {
-        BST<Long, String> bst = new BST<Long, String>();
+        BST<Integer, String> bst = new BST<Integer, String>();
 
-        bst.put(1L, "1");
-        bst.put(2L, "2");
-        bst.put(4L, "4");
-        bst.put(5L, "5");
-        bst.put(0L, "0");
+        bst.put(1, "1");
+        bst.put(2, "2");
+        bst.put(4, "4");
+        bst.put(5, "5");
+        bst.put(0, "0");
 
         System.out.print("先序遍历：");
         bst.recursivePreOrder();
@@ -21,13 +21,14 @@ public class BSTTest {
         bst.recursivePostOrder();
         System.out.println();
 
-        System.out.println("树大小:" + bst.size());
-        System.out.println("最小值:" + bst.min());
-        System.out.println("最大值:" + bst.max());
-        System.out.println("向上取数2.5:" + bst.floor((long) 2.5));
-        System.out.println("向下取数0.5:" + bst.ceil((long) 0.5));
-        
-        System.out.println("指定位置的元素3："+ bst.select(3));
-        System.out.println("元素3的位置:" + bst.rank(3L));
+        System.out.println("二叉树大小:" + bst.size());
+        System.out.println("二叉树最小值:" + bst.min());
+        System.out.println("二叉树最大值:" + bst.max());
+        System.out.println("二叉树向上取数3:" + bst.floor(3));
+        System.out.println("二叉树向下取数3:" + bst.ceil(3));
+
+        System.out.println("二叉树指定位置的元素3：" + bst.select(3));
+        System.out.println("二叉树元素3的位置:" + bst.rank(3));
+        System.out.println("二叉树高度：" + bst.heigh());
     }
 }
